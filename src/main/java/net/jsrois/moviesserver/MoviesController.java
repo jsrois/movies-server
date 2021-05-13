415135e129cbfef984a18e79813bed39186cde58
Javi <javi@jsrois.net>
2021-05-13 10:17:29 +0200

    Adding movies

--- src/main/java/net/jsrois/moviesserver/MoviesController.java
+++ src/main/java/net/jsrois/moviesserver/MoviesController.java
@@ -1,8 +1,14 @@
 package net.jsrois.moviesserver;
 
+import org.apache.coyote.Response;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.view.RedirectView;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @RestController
@@ -11,17 +17,32 @@ public class MoviesController {
     private List<Movie> movies;
 
     public MoviesController() {
-        movies = List.of(
-                new Movie("Kingdom of Heaven", 2005),
-                new Movie("Judas and the Black Messiah", 2021),
-                new Movie("Toy Story", 1995)
-        );
+        movies = new ArrayList<>();
     }
 
     @GetMapping("/movies")
     public List<Movie> allMovies() {
         return movies;
     }
+
+    @PostMapping("/movies")
+    public RedirectView addNewMovie(Movie movie) {
+        movies.add(movie);
+        return new RedirectView("/");
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 }

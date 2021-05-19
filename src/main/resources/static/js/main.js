e0353899ce17f55ac937188e93f2a78f802e360b
Javi <javi@jsrois.net>
2021-05-19 10:14:56 +0200

    Adding remove button in each movie

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -2,26 +2,26 @@ const moviesSection = document.querySelector("#movies-section");
 
 // 3)
 function showMovies(movies) {
-    moviesSection.innerHTML="";
-    movies.forEach((movie, i) => {
+    moviesSection.innerHTML = "";
+    movies.forEach((movie, position) => {
         const htmlElement = document.createElement("div");
         htmlElement.innerHTML = `<div class="movie">
+            <div 
+                class="close-button" 
+                onclick="fetch(\`/movies/${position}\`, { method: 'DELETE'}).then(reloadMovies)">x</div>
             <p class="title">${movie.title} (${movie.year})</p>
         </div>`;
-        htmlElement.addEventListener("click", function(){
-            fetch(`/movies/${i}`, { method: 'DELETE'}).then(loadMovies)
-        })
         moviesSection.appendChild(htmlElement)
     })
 }
 
 
 // 2)
-let loadMovies = () => {
+let reloadMovies = () => {
     fetch("/movies")
         .then(r => r.json())
         .then(showMovies)
 };
 
 // 1)
-document.addEventListener("DOMContentLoaded", loadMovies)
+document.addEventListener("DOMContentLoaded", reloadMovies)
